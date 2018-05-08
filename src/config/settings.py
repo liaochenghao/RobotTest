1166f4191102b7e14ac52c4bb702f06587ff30f4
liaochenghao <1295316148@qq.com>
2018-05-08 10:07:10 +0800

    finally modify

--- src/config/settings.py
+++ src/config/settings.py
@@ -5,15 +5,15 @@ import pymysql
 APP_ID = 'wx5ac997cbc0ebe57b'
 APP_SECRET = '44c699343b2f41c0b1b069e01411465d'
 
-db_config = {
-    'host': '42.51.8.152',
-    'port': 3306,
-    'db': 'stu_system',
-    'user': 'root',
-    'password': '1q2w3e4r!Q',
-    'cursorclass': pymysql.cursors.DictCursor,
-    'charset': 'utf8'
-}
+# db_config = {
+#     'host': '42.51.8.152',
+#     'port': 3306,
+#     'db': 'stu_system',
+#     'user': 'root',
+#     'password': '1q2w3e4r!Q',
+#     'cursorclass': pymysql.cursors.DictCursor,
+#     'charset': 'utf8'
+# }
 
 # REDIS_CONFIG = {
 #     'host': '47.92.115.126',
--- utils/functions.py
+++ utils/functions.py
@@ -2,50 +2,50 @@
 import pymysql
 # import redis
 
-from src.config.settings import db_config, REDIS_CONFIG
+# from src.config.settings import db_config，REDIS_CONFIG
 
 
-redis_valid_time = 1 * 60 * 60
+# redis_valid_time = 1 * 60 * 60
 
 
-def my_custom_sql(query, is_query=True):
-    conn = pymysql.connect(**db_config)
-    with conn.cursor() as cursor:
-        cursor.execute(query)
-        if is_query:
-            data = cursor.fetchall()
-        else:
-            conn.commit()
-            data = {'id': cursor.lastrowid}
-    conn.close()
-    return data
+# def my_custom_sql(query, is_query=True):
+#     conn = pymysql.connect(**db_config)
+#     with conn.cursor() as cursor:
+#         cursor.execute(query)
+#         if is_query:
+#             data = cursor.fetchall()
+#         else:
+#             conn.commit()
+#             data = {'id': cursor.lastrowid}
+#     conn.close()
+#     return data
 
 
-class RedisClient:
-
-    @property
-    def redis_client(self):
-        client = redis.Redis(host=REDIS_CONFIG['host'], port=REDIS_CONFIG['port'])
-        return client
-
-    def get_instance(self, key, delete_cache=False):
-        redis_instance = self.redis_client.get(key)
-        if not redis_instance:
-            return None
-        try:
-            res = eval(redis_instance)
-        except:
-            res = str(redis_instance, encoding='utf-8')
-        if delete_cache:            # 默认获取redis数据之后立即删除数据
-            self.redis_client.delete(key)
-        return res
-
-    def set_instance(self, key, value, default_valid_time=redis_valid_time):
-        self.redis_client.set(key, value, default_valid_time)
-        return
-
-    def delete_instance(self, *key):
-        self.redis_client.delete(*key)
-
-
-redis_client = RedisClient()
+# class RedisClient:
+#
+#     @property
+#     def redis_client(self):
+#         client = redis.Redis(host=REDIS_CONFIG['host'], port=REDIS_CONFIG['port'])
+#         return client
+#
+#     def get_instance(self, key, delete_cache=False):
+#         redis_instance = self.redis_client.get(key)
+#         if not redis_instance:
+#             return None
+#         try:
+#             res = eval(redis_instance)
+#         except:
+#             res = str(redis_instance, encoding='utf-8')
+#         if delete_cache:            # 默认获取redis数据之后立即删除数据
+#             self.redis_client.delete(key)
+#         return res
+#
+#     def set_instance(self, key, value, default_valid_time=redis_valid_time):
+#         self.redis_client.set(key, value, default_valid_time)
+#         return
+#
+#     def delete_instance(self, *key):
+#         self.redis_client.delete(*key)
+#
+#
+# redis_client = RedisClient()
